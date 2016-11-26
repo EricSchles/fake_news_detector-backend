ea7b293f6f5136ea0d966cbc953b11c68ca9ba9f
Eric Schles <ericschles@gmail.com>
2016-11-26 12:01:19 -0500

    adding secret key

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,11 @@ import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SECRET_KEY"] = "testing"
 #app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://eric_s:1234@localhost/testing"
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
-cors = CORS(app)
+cors = CORS(app, resources={"/send_data":{"origins":"serene-reef-39081.herokuapp.com"})
 
 app.config["CORS_HEADERS"] = 'Content-Type'
 manager = Manager(app)
